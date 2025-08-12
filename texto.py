68efab81fad47f105d8a0965d0145435ee69209a
antoniasoeiro <mariaantonia.delimasoeiro.ismart@gmail.com>
2025-08-12 22:55:39 +0000

    sorteio e média

--- texto.py
+++ texto.py
@@ -1,3 +1,12 @@
+import random
+
 def cumprimento(texto):
     return f"Olá, {texto}!"
 print(cumprimento("Maria Antônia Soeiro"))
+
+def sorteio():
+    numeros = [random.randint(1,100) for _ in range(7)]
+    media = sum(numeros) / len(numeros)
+    return f"Números sorteados: {numeros}, Média: {media:.2f}"
+
+sorteio()
